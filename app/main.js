96b728db094247797356ef844a0734ea16c11092
Ben Lavender <bhuga@github.com>
2015-11-19 18:35:35 -0800

    add a menu

--- app/main.js
+++ app/main.js
@@ -4,8 +4,10 @@ var scanner = require('portscanner');
 var BrowserWindow = require('browser-window');
 var Menu = require("menu");
 var env = require('./vendor/electron_boilerplate/env_config');
+var menuTemplate = require('./menu_template')(app);
 var devHelper = require('./vendor/electron_boilerplate/dev_helper');
 var windowStateKeeper = require('./vendor/electron_boilerplate/window_state');
+var shell = require('shell');
 
 var mainWindow;
 
@@ -16,7 +18,6 @@ var mainWindowState = windowStateKeeper('main', {
 });
 
 app.on('ready', function () {
-
     mainWindow = new BrowserWindow({
         x: mainWindowState.x,
         y: mainWindowState.y,
@@ -38,30 +39,12 @@ app.on('ready', function () {
 
       mainWindow.webContents.on('new-window', function(e, url) {
         e.preventDefault();
-        require('shell').openExternal(url);
+        shell.openExternal(url);
       });
 
       mainWindow.loadUrl('https://my.slack.com/ssb');
-      var template = [{
-          label: "Application",
-          submenu: [
-              { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-              { type: "separator" },
-              { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-          ]}, {
-          label: "Edit",
-          submenu: [
-              { label: "Undo", accelerator: "Command+Z", selector: "undo:" },
-              { label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
-              { type: "separator" },
-              { label: "Cut", accelerator: "Command+X", selector: "cut:" },
-              { label: "Copy", accelerator: "Command+C", selector: "copy:" },
-              { label: "Paste", accelerator: "Command+V", selector: "paste:" },
-              { label: "Select All", accelerator: "Command+A", selector: "selectAll:" }
-          ]}
-      ];
 
-      Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+      Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
     });
 
     if (env.name === 'development') {
---
+++ app/menu_template.js
@@ -0,0 +1,156 @@
+'use strict';
+console.log("in menu template");
+module.exports = function(app) {
+  var template = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          selector: 'undo:'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          selector: 'redo:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          selector: 'cut:'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          selector: 'copy:'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          selector: 'paste:'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          selector: 'selectAll:'
+        },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Ctrl+Command+F';
+            else
+              return 'F11';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Alt+Command+I';
+            else
+              return 'Ctrl+Shift+I';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.toggleDevTools();
+          }
+        },
+      ]
+    },
+    {
+      label: 'Window',
+      selector: 'window:',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          selector: 'minimize:'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          selector: 'close:'
+        },
+      ]
+    }
+  ];
+
+  if (process.platform == 'darwin') {
+    var name = "Slack Wrapper"
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          label: 'About ' + name,
+          selector: 'about:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Services',
+          selector: 'services:',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Hide ' + name,
+          accelerator: 'Command+H',
+          selector: 'hide:'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          selector: 'hideothers:'
+        },
+        {
+          label: 'Show All',
+          selector: 'unhide:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function() { app.quit(); }
+        },
+      ]
+    });
+    // Window menu.
+    template[3].submenu.push(
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        selector: 'front:'
+      }
+    );
+  }
+  return template;
+};
